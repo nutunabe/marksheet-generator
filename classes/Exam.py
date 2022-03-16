b65a8fc6c2ab940d941adcb1d935f233f012debd
nutunabe <baiytaan@gmail.com>
2022-03-16 18:35:12 +0900

    asdasd

--- classes/Exam.py
+++ classes/Exam.py
@@ -11,9 +11,9 @@ class Exam:
     lecturerFio: str
 
     def __init__(self, value_subject: Subject, value_examDate: date, value_year: str, value_lecturerFio: str):
-        # self.subject = value_subject
-        # self.examDate = value_examDate
-        self.subject = value_examDate
-        self.examDate = value_subject
+        self.subject = value_subject
+        self.examDate = value_examDate
+        # self.subject = value_examDate
+        # self.examDate = value_subject
         self.year = value_year
         self.lecturerFio = value_lecturerFio
